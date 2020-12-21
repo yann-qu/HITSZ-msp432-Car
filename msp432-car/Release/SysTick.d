070c28b152736ca6e4e92d443a0033844bfe7c6d
yann-qu <573155264@qq.com>
2020-12-21 15:31:58 +0800

    慢速跑完全程。注意：必须用debug模式编译

---
+++ msp432-car/Release/SysTick.d
@@ -0,0 +1,41 @@
+# FIXED
+
+SysTick.obj: E:/Code/MSP432/inc/SysTick.c
+SysTick.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h
+SysTick.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h
+SysTick.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h
+SysTick.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h
+SysTick.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h
+SysTick.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h
+SysTick.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h
+SysTick.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h
+SysTick.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h
+SysTick.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h
+SysTick.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/msp.h
+SysTick.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/msp432p401r.h
+SysTick.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/msp_compatibility.h
+SysTick.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/msp432p401r_classic.h
+SysTick.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+SysTick.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+SysTick.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+SysTick.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/system_msp432p401r.h
+
+E:/Code/MSP432/inc/SysTick.c: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h: 
+C:/ti/ccs1010/ccs/ccs_base/arm/include/msp.h: 
+C:/ti/ccs1010/ccs/ccs_base/arm/include/msp432p401r.h: 
+C:/ti/ccs1010/ccs/ccs_base/arm/include/msp_compatibility.h: 
+C:/ti/ccs1010/ccs/ccs_base/arm/include/msp432p401r_classic.h: 
+C:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+C:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/ti/ccs1010/ccs/ccs_base/arm/include/system_msp432p401r.h: 
