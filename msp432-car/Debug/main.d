00ed0e09be05c981b99a5ec570735059549ebcc0
yann-qu <573155264@qq.com>
2020-12-22 17:37:13 +0800

    不完善的版本。尝试提高速度但是问题挺大的

--- msp432-car/Debug/main.d
+++ msp432-car/Debug/main.d
@@ -1,24 +1,24 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h
-main.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h
-main.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h
-main.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h
-main.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h
-main.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h
-main.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h
-main.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h
-main.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h
-main.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h
-main.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/msp.h
-main.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/msp432p401r.h
-main.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/msp_compatibility.h
-main.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/msp432p401r_classic.h
-main.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
-main.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
-main.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
-main.obj: C:/ti/ccs1010/ccs/ccs_base/arm/include/system_msp432p401r.h
+main.obj: D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h
+main.obj: D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h
+main.obj: D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h
+main.obj: D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h
+main.obj: D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h
+main.obj: D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h
+main.obj: D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h
+main.obj: D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h
+main.obj: D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h
+main.obj: D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h
+main.obj: D:/ti/ccs1010/ccs/ccs_base/arm/include/msp.h
+main.obj: D:/ti/ccs1010/ccs/ccs_base/arm/include/msp432p401r.h
+main.obj: D:/ti/ccs1010/ccs/ccs_base/arm/include/msp_compatibility.h
+main.obj: D:/ti/ccs1010/ccs/ccs_base/arm/include/msp432p401r_classic.h
+main.obj: D:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+main.obj: D:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+main.obj: D:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+main.obj: D:/ti/ccs1010/ccs/ccs_base/arm/include/system_msp432p401r.h
 main.obj: ../../inc/Bump.h
 main.obj: ../../inc/Clock.h
 main.obj: ../../inc/CortexM.h
@@ -28,24 +28,24 @@ main.obj: ../../inc/PWM.h
 main.obj: ../../inc/Reflectance.h
 
 ../main.c: 
-C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h: 
-C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h: 
-C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h: 
-C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h: 
-C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h: 
-C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h: 
-C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h: 
-C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h: 
-C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h: 
-C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h: 
-C:/ti/ccs1010/ccs/ccs_base/arm/include/msp.h: 
-C:/ti/ccs1010/ccs/ccs_base/arm/include/msp432p401r.h: 
-C:/ti/ccs1010/ccs/ccs_base/arm/include/msp_compatibility.h: 
-C:/ti/ccs1010/ccs/ccs_base/arm/include/msp432p401r_classic.h: 
-C:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/core_cm4.h: 
-C:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
-C:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
-C:/ti/ccs1010/ccs/ccs_base/arm/include/system_msp432p401r.h: 
+D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h: 
+D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h: 
+D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h: 
+D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h: 
+D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h: 
+D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h: 
+D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h: 
+D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h: 
+D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h: 
+D:/ti/ccs1010/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h: 
+D:/ti/ccs1010/ccs/ccs_base/arm/include/msp.h: 
+D:/ti/ccs1010/ccs/ccs_base/arm/include/msp432p401r.h: 
+D:/ti/ccs1010/ccs/ccs_base/arm/include/msp_compatibility.h: 
+D:/ti/ccs1010/ccs/ccs_base/arm/include/msp432p401r_classic.h: 
+D:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/core_cm4.h: 
+D:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+D:/ti/ccs1010/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+D:/ti/ccs1010/ccs/ccs_base/arm/include/system_msp432p401r.h: 
 ../../inc/Bump.h: 
 ../../inc/Clock.h: 
 ../../inc/CortexM.h: 
--- msp432-car/main.c
+++ msp432-car/main.c
@@ -94,7 +94,7 @@ int32_t position;
 
 void reflactance(void) {
 
-    Data     = Reflectance_Read(1000);
+    Data     = Reflectance_Read(500);
     position = Reflectance_Position(Data);
 
     //* 在正中
@@ -116,14 +116,24 @@ void reflactance(void) {
         TimedPause(100);
     }
     //*
-    else if ((position > 0) && (position < 237)) {
+    else if ((position > 0) && (position < 142)) {
         // Motor_Forward((3000 - 8 * position), 3700);  // 左转
-        Motor_Forward((3000 - 8 * position), 4000);  // 左转
+        Motor_Forward((3500 - 8 * position), 4000);  // 左转
     }
     //*
-    else if ((position < 0) && (position > -237)) {
+    else if ((position > 142) && (position < 237)) {
+        // Motor_Forward((3000 - 8 * position), 3700);  // 左转
+        Motor_Forward((4000 - 8 * position), 5000);  // 左转
+    }
+    //*
+    else if ((position < 0) && (position > -142)) {
         // Motor_Forward(3700, (3000 + 8 * position));  // 右转
-        Motor_Forward(4000, (3000 + 8 * position));  // 右转
+        Motor_Forward(4000, (3500 + 8 * position));  // 右转
+    }
+    //*
+    else if ((position < -142) && (position > -237)) {
+        // Motor_Forward(3700, (3000 + 8 * position));  // 右转
+        Motor_Forward(5000, (4000 + 8 * position));  // 右转
     }
     //* T型路口
     if (Data == 0xFF) {
@@ -134,7 +144,7 @@ void reflactance(void) {
     }
     //* 直角右转
     if ((Data == 0xF8) || (Data == 0xF0) || (Data == 0xE0)) {
-        Data = Reflectance_Read(1000);
+        Data = Reflectance_Read(500);
         TimedPause(5);
         if ((Data == 0xF8) || (Data == 0xF0) || (Data == 0xE0)) {
             Motor_Forward(VELOCITY0, VELOCITY0);
@@ -145,7 +155,7 @@ void reflactance(void) {
     }
     //* 直角左转
     if ((Data == 0x1F) || (Data == 0x0F) || (Data == 0x07)) {
-        Data = Reflectance_Read(1000);
+        Data = Reflectance_Read(500);
         TimedPause(5);
         if ((Data == 0x1F) || (Data == 0x0F) || (Data == 0x07)) {
             Motor_Forward(VELOCITY0, VELOCITY0);
